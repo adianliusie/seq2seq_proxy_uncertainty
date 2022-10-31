fa27353ea8e1f992f0813ba48a18d2bc40a9a7bb
Adian Liusie <adianliusie98@gmail.com>
2022-10-31 15:16:25 +0000

    supressed tokenizer warning

--- models/tokenizers.py
+++ models/tokenizers.py
@@ -11,9 +11,9 @@ def load_tokenizer(system: str) -> PreTrainedTokenizer:
     Downloads and returns the relevant pretrained seq2seq tokenizer from huggingface
     """
     if system == 't5-small': 
-        return T5TokenizerFast.from_pretrained("t5-small")
+        return T5TokenizerFast.from_pretrained("t5-small", model_max_length=512)
     elif system == 't5-base': 
-        return T5TokenizerFast.from_pretrained("t5-base")
+        return T5TokenizerFast.from_pretrained("t5-base", model_max_length=512)
     elif system == 't5-large': 
-        return T5TokenizerFast.from_pretrained("t5-large")
+        return T5TokenizerFast.from_pretrained("t5-large", model_max_length=512)
     raise ValueError("{} is invalid".format(system))
